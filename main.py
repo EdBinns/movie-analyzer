8dba3e744a02a9e716223db3f00ed3d3c1ebb1a8
unknown <morabarrantes.andres@gmail.com>
2021-10-12 10:49:53 -0600

    hilos, timer y imagenes generadas

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
 import argparse
-import time
 import datetime
 import threading
 
@@ -9,10 +8,10 @@ import threading
 parser = argparse.ArgumentParser()
 parser.add_argument('--play_video', help="Tue/False", default=False)
 parser.add_argument('--image', help="Tue/False", default=False)
-parser.add_argument('--video_path', help="Path of video file", default="videos/fire1.mp4")
+parser.add_argument('--video_path', help="Path of video file", default="videos/pistol.mp4")
+parser.add_argument('--video_path2', help="Path of video file", default="videos/rifle.mp4")
 parser.add_argument('--image_path', help="Path of image to detect objects", default="Images/bicycle.jpg")
 parser.add_argument('--verbose', help="To print statements", default=True)
-parser.add_argument('--use_gpu', help='Use GPU (OpenCV must be compiled for GPU).', default=False,)
 parser.add_argument('--weights', help='Path to model weights', default='yolov3.weights')
 parser.add_argument('--config', help='Path to configuration file', default='yolov3.cfg')
 
@@ -23,10 +22,6 @@ args = parser.parse_args()
 def load_yolo():
     #net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
     net = cv2.dnn.readNetFromDarknet(args.config, args.weights)
-    if args.use_gpu:
-        print('Using GPU')
-        net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
-        net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
 
     classes = []
     with open("obj.names", "r") as f:
@@ -76,7 +71,6 @@ def draw_labels(boxes, confs, colors, class_ids, classes, img, fps):
     global imageNumber
     global lastSavedImage
     global frameFlag
-    #print(imageNumber)
     imageNumber += 1
     indexes = cv2.dnn.NMSBoxes(boxes, confs, 0.5, 0.4)
     font = cv2.FONT_HERSHEY_PLAIN
@@ -92,7 +86,6 @@ def draw_labels(boxes, confs, colors, class_ids, classes, img, fps):
             if label in ["Rifle", "Gun", "Fire"]:
                 segundos = imageNumber / fps
                 tiempo = str(datetime.timedelta(seconds=round(segundos))).replace(":", "-")
-                print(tiempo)
                 if (frameFlag >= 120):
                     cv2.imwrite('D:\\Carpeta\\' + tiempo + '.jpg', img)
                     frameFlag = 0
@@ -107,48 +100,43 @@ def start_video(video_path):
     model, classes, colors, output_layers = load_yolo()
     cap = cv2.VideoCapture(video_path)
     FPS = int(cap.get(cv2.CAP_PROP_FPS))
-    lista = []
     contador = 0
     while True:
         G, frame = cap.read()
         if not G:
             break
-        lista.append(frame)
         print(contador)
-        contador +=1
+        contador += 1
 
         height, width, channels = frame.shape
         blob, outputs = detect_objects(frame, model, output_layers)
         boxes, confs, class_ids = get_box_dimensions(outputs, height, width)
         draw_labels(boxes, confs, colors, class_ids, classes, frame, FPS)
-
         key = cv2.waitKey(1)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+
     cap.release()
-
-
-
-    #t1 = threading.Thread(target=image_detect, args=(lista[:25],))
-    #t2 = threading.Thread(target=image_detect, args=(lista[26:50],))
-
-    #t1.start()
-    #t2.start()
-
-    #t1.join()
-    #t2.join()
-
-
-    #image_detect(lista[:200])
-
-
 
 if __name__ == '__main__':
     video_play = args.play_video
     image = args.image
     if video_play:
         video_path = args.video_path
+        video_path2 = args.video_path2
         if args.verbose:
             print('Opening ' + video_path + " .... ")
-        start_video(video_path)
+
+
+        t1 = threading.Thread(target=start_video, args=(video_path,))
+        t2 = threading.Thread(target=start_video, args=(video_path2,))
+
+        start_time = datetime.datetime.now()
+        t1.start()
+        t2.start()
+        t1.join()
+        t2.join()
+        end_time = datetime.datetime.now()
+        print('Duration: {}'.format(end_time - start_time))
+
     cv2.destroyAllWindows()
